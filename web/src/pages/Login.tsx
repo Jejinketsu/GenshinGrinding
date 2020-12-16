c866565d6d8795436005ca3f422518e44e69d96e
Dark Tech <cliciosa14@gmail.com>
2020-12-16 14:22:53 -0300

    refactor: refactoring app code

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,32 +1,33 @@
-import React from 'react';
-
-
 import '../styles/pages/Login.css';
 
-import Logo from '../images/logo-genshin-2.png';
+import logo from '../images/logo-genshin-2.png';
 
+function Login() {
+  return (
+    <div id='page-login'>
+      <div className='banner-genshin'></div>
 
-function Login(){
-    return(
-        <div id="page-login">   
-            <div className="banner-genshin"></div>
-            <div className="box-login">
-                <img src={Logo} alt="" id="logo-genshin-login"/>
-                <form className="form">
-                    <div className="input-block">                    
-                        <label htmlFor="">Usuário</label>
-                        <input type="text"/>
-                        <label htmlFor="">Senha</label>
-                        <input type="password"/>
-                        
-                        <button className="enter-app">Login</button>
-                        
-                    </div>
-                    <a href="/register"><li id="clique-to-register">Clique aqui para se cadastrar</li></a>
-                </form>
-            </div>
-        </div>
-    );
+      <div className='box-login'>
+        <img src={logo} alt='' id='logo-genshin-login' />
+
+        <form className='form'>
+          <div className='input-block'>
+            <label htmlFor=''>Usuário</label>
+            <input type='text' />
+
+            <label htmlFor=''>Senha</label>
+            <input type='password' />
+
+            <button className='enter-app'>Login</button>
+          </div>
+
+          <a href='/register'>
+            <li id='clique-to-register'>Clique aqui para se cadastrar</li>
+          </a>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
