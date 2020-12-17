5e2b46ca7580fada1b3d84db8f0f78231138ed52
Dark Tech <cliciosa14@gmail.com>
2020-12-17 11:28:16 -0300

    feat: creating TopBar component

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,22 +2,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Domain from './pages/Domain';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' exact component={Login} />
+        <Route path='/' component={Login} exact />
         <Route path='/register' component={Register} />
+        <Route path='/domain' component={Domain} />
       </Switch>
     </BrowserRouter>
   );
 }
 
 export default Routes;
-
-/*
-  Essa propriedade exact é para que ele só mostre o Landing caso seja exatamente a rota só com o /
-  Pois se não toda rota que começa com / ele irá chamar
-  O Switch faz com que só uma rota seja chamada
-*/
