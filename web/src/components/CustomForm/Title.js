2eae1686045588c9d8a0cc9f4ab53b5c8d1f078b
Lucas Vinicius <lucass.lv.lv@gmail.com>
2021-07-03 19:34:08 -0300

    Custom input inteligente

---
+++ web/src/components/CustomForm/Title.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../styles/components/Title.css";
+
+const Title = ({ title }) => {
+  return <p className="title">{title}</p>;
+};
+
+export default Title;
--- web/src/pages/AdminForms/ItensForms.js
+++ web/src/pages/AdminForms/ItensForms.js
@@ -1,13 +1,20 @@
 import React from "react";
+import "../../styles/components/DashboardForms.css";
 import Input from "../../components/CustomForm/Input";
 import useForm from "../../components/CustomHooks/useForm";
+import Title from "../../components/CustomForm/Title";
 
 const ItensForms = () => {
-  const itemNome = useForm("number");
+  const itemName = useForm("text");
+  const itemDescription = useForm("text");
 
   return (
-    <div>
-      <Input label="Nome" type="text" name="itemNome" {...itemNome} />
+    <div className="CadForm">
+      <Title title="Itens" />
+      <form>
+        <Input label="Name" type="text" name="itemName" {...itemName} />
+        <Input label="Description" type="text" name="itemDescription" {...itemDescription} />
+      </form>
     </div>
   );
 };
